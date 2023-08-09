353edc4e37e2ad0312f2512291013b68e4f34c55
David Marsen Purba <dmonap@student.telkomuniversity.ac.id>
2023-08-09 11:04:39 +0700

    mouse event

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
 const app = Vue.createApp({
-    data(){
+    data() {
         return {
             tittle: 'Dr. James',
             episode: 1,
+            event: 'event',
+            x: 0,
+            y: 0,
         }
     },
-   methods: {
-    getResult(episode){
-        if (episode === 1) {
-            return 'Diceritakan sebuah anak yang bertekad untuk membalaskan dendam karena kematian orangtuanya';
-        } else if (episode === 2){
-           return 'Dia berusaha keras untuk menjadi seorang doktor demi memenuhi rencana balas dendamnya';
-        } else {
-           return 'COMING SOON';
-        }
-    },
-    
-    nextEpisode(){
-        this.episode++
-    },
+    methods: {
+        getResult(episode) {
+            if (episode === 1) {
+                return 'Diceritakan sebuah anak yang bertekad untuk membalaskan dendam karena kematian orangtuanya';
+            } else if (episode === 2) {
+                return 'Dia berusaha keras untuk menjadi seorang doktor demi memenuhi rencana balas dendamnya';
+            } else {
+                return 'COMING SOON';
+            }
+        },
 
-    previousEpisode(){
-        this.episode--
+        nextEpisode() {
+            this.episode++
+        },
+
+        previousEpisode() {
+            this.episode--
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousePosition(e) {
+           this.x = e.offsetX
+           this.y = e.offsetY
+        }
     }
-   }
 })
 
 
